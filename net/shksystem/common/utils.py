f2d5aef2ea82ca4c8a8f9f453f27da72eed187c4
Sarfraz Kapasi <sarfraz@variablentreprise.com>
2015-03-23 10:26:15 +0100

    0.1.4.1

--- net/shksystem/common/utils.py
+++ net/shksystem/common/utils.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-#@(#)----------------------------------------------------------------------
-#@(#) OBJET            : Process helpers
-#@(#)----------------------------------------------------------------------
-#@(#) AUTEUR           : Sarfraz Kapasi
-#@(#) DATE DE CREATION : 03.01.2015
-#@(#) LICENSE          : GPL-3
-#@(#)----------------------------------------------------------------------
 
+""" #@(#)------------------------------------------------------------------
+    #@(#) OBJET            : Process helpers
+    #@(#)------------------------------------------------------------------
+    #@(#) AUTEUR           : Sarfraz Kapasi
+    #@(#) DATE DE CREATION : 03.01.2015
+    #@(#) LICENSE          : GPL-3
+    #@(#)------------------------------------------------------------------
+"""
 #==========================================================================
 #
 # WARNINGS
@@ -20,21 +21,19 @@
 
 # standard
 import os
-import sys
 import binascii
 import random
 import datetime
 import logging, logging.handlers
-from subprocess                 import call
+from subprocess import call
 import shutil
 import tempfile
 import csv
-from pprint                     import pprint
 # installed
-from sqlalchemy                 import create_engine
-from sqlalchemy.orm             import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 # custom
-from net.shksystem.common.error import *
+from net.shksystem.common.error import FileNotFound, CommandFailure
 
 #==========================================================================
 # Environment/Static variables
@@ -44,72 +43,74 @@ from net.shksystem.common.error import *
 # Classes/Functions
 #==========================================================================
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
-## This function add console and file handlers to a Logger object
-# initLogger :: FilePath
-#            -> String
-#            -> IO Logger
 def init_logger(log_path, log_level):
-
+    """ This function add console and file handlers to a Logger object
+        init_logger :: FilePath
+                   -> String
+                   -> IO Logger
+    """
     loggo = logging.getLogger()
     # setting formatter
     formater = logging.Formatter("%(asctime)s %(name)s %(levelname)s - %(message)s", "%d-%m %H:%M")
     # configuring rotation
-    hd = logging.handlers.RotatingFileHandler(log_path, maxBytes=5000000, backupCount=5)
-    hd.setFormatter(formater)
+    rotate_file_handler = logging.handlers.RotatingFileHandler(log_path, maxBytes=5000000, backupCount=5)
+    rotate_file_handler.setFormatter(formater)
     # adding console output
-    ch = logging.StreamHandler()
-    ch.setFormatter(formater)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formater)
     # configuring logger
     loggo.setLevel(log_level)
-    loggo.addHandler(hd)
-    loggo.addHandler(ch)
+    loggo.addHandler(rotate_file_handler)
+    loggo.addHandler(console_handler)
 
     return loggo
 
-## This function calls pdflatex on LaTEX source file
-#    and performs basic cleanup afterwards
-# compileTex :: FilePath
-#            -> IO ()
 def compile_tex(tex_source_file):
+    """ This function calls pdflatex on LaTEX source file and performs basic cleanup afterwards
+        compileTex :: FilePath
+                   -> IO ()
+    """
     if not os.path.isfile(tex_source_file):
-        logger.warn('Input file does not exist'.format(tex_source_file,))
+        log.warn('Input file does not exist : %s.', tex_source_file)
         raise FileNotFound
     tex_file_name = os.path.splitext(tex_source_file)[0]
-    exts          = ['aux', 'log']
+    exts = ['aux', 'log']
 
-    logger.info('Calling pdflatex on {0}.'.format(tex_source_file,))
+    log.info('Calling pdflatex on %s.', tex_source_file)
     code = call(['pdflatex', '-interaction=nonstopmode', tex_source_file, '>', os.devnull])
     if code != 0:
-        logger.warn('Compilation failure.')
+        log.warn('Compilation failure.')
         raise CommandFailure
 
     for ext in exts:
         final_file = '{0}{1}{2}'.format(tex_file_name, os.extsep, ext)
         if os.path.isfile(final_file):
-            logger.info('Deleting {0}.'.format(final_file,))
+            log.info('Deleting %s.', final_file)
             os.unlink(final_file)
 
-    logger.info('Compilation success.')
+    log.info('Compilation success.')
 
-## This function gets current date and time with the following format:
-#    dd/mm/yy:HH:MM
-# get_current_timestamp :: IO String
 def get_current_timestamp():
+    """ This function gets current date and time with the following format: dd/mm/yy:HH:MM
+        get_current_timestamp :: IO String
+    """
     ret = datetime.datetime.now().isoformat()
     return ret
 
-## This function generate random n lenght token
-# gen_random_token :: IO String
-def gen_random_token(n):
-    ret = binascii.hexlify(os.urandom(n))
+def gen_random_token(token_len):
+    """ This function generate random token_len lenght token
+        gen_random_token :: IO String
+    """
+    ret = binascii.hexlify(os.urandom(token_len))
     return ret
 
-## This function returns the nth element of a list
-# get_random_elem :: [a]
-#                 -> IO a
 def get_random_elem(lst):
+    """ This function returns the nth element of a list
+        get_random_elem :: [a]
+                        -> IO a
+    """
     ret = random.choice(lst)
     return ret
 
@@ -121,40 +122,39 @@ def replace_in_file(fic, patt, subst):
                         -> IO ()
     """
     try:
-        fh, pabs = tempfile.mkstemp()
-        nfic     = open(pabs,'w')
-        ofic     = open(fic)
+        fich, temp_fic = tempfile.mkstemp()
+        nfic = open(temp_fic, 'w')
+        ofic = open(fic)
         for line in ofic:
             nfic.write(line.replace(patt, subst))
         os.remove(fic)
-        shutil.move(pabs, fic)
-    except:
-        logger.exception('Replace of patern {0} by {1} failed on file {2}.'
-                     .format(patt, subst, fic))
+        shutil.move(temp_fic, fic)
+    except OSError:
+        log.exception('Replace of patern %s by %s failed on file %s.', patt, subst, fic)
     finally:
         nfic.close()
         ofic.close()
-        os.close(fh)
+        os.close(fich)
 
 def remove_existing_fics(list_of_fics):
     """ This function unlink a list of file given that they exist on filesystem
         remove_existing_fics :: [FilePath] -- ^ List of files to be removed
                              -> IO ()
     """
-    for p in list_of_fics:
+    for path in list_of_fics:
         if os.path.isfile:
-            os.unlink(p)
+            os.unlink(path)
 
 def remove_csv_duplicates(csv_fic):
     """ This function recreate a file while removing duplicates lines in it
         remove_csv_duplicates :: FilePath
                               -> IO ()
     """
-    if (os.path.isfile(csv_fic)):
+    if os.path.isfile(csv_fic):
         try:
-            fh, tmp_fic = tempfile.mkstemp()
-            in_file     = open(csv_fic, 'r')
-            out_file    = open(tmp_fic, 'w')
+            fich, tmp_fic = tempfile.mkstemp()
+            in_file = open(csv_fic, 'r')
+            out_file = open(tmp_fic, 'w')
             seen = set() # fast O(1) amortized lookup
 
             for line in in_file:
@@ -162,100 +162,17 @@ def remove_csv_duplicates(csv_fic):
                     continue # skip duplicate
                 seen.add(line)
                 out_file.write(line)
-        except:
-            logger.exception('Cannot remove duplicate in csv file : {0}'.format(csv_fic,))
+        except OSError:
+            log.exception('Cannot remove duplicate in csv file : %s.', csv_fic)
         finally:
             in_file.close()
             out_file.close()
-            os.close(fh)
+            os.close(fich)
 
         os.unlink(csv_fic)
         shutil.move(tmp_fic, csv_fic)
     else:
-        logger.error('Csv file to be filtered does not exist : {0}.'.format(csv_fic,))
-
-def get_uq_comb_in_csv(csv_fic, uq_table):
-    """ This function takes a csv file and an uniqueness table and returns
-        the number of unique elements statisfying the table
-        get_uq_comb_in_csv :: FilePath
-                           -> [Integer]
-                           -> IO Integer
-    """
-    ret = 0
-
-    try:
-        csv_hand  = open(csv_fic)
-        data      = [k for k in csv.reader(csv_hand)]
-        data_dups = []
-        data_len  = len(data)
-        i         = 0
-        j         = 0
-        while i < len(data):
-            while j < len(data):
-                # Test of value n with n+1
-                # If elem z of n doesn't equal elem z of n+1 then rows aren't equal
-                row_eq = False if i == j else all(map(lambda x: data[i][x] == data[j][x], uq_table))
-                # The two rows aren't equal that's good, let's continue else this element isn't unique
-                if not row_eq:
-                    j += 1
-                else:
-                    del data[i]
-                # Now we got to the end without getting any break
-                if j == (len(data) -1):
-                    ret += 1
-            i += 1
-            j = 0
-    except:
-        logger.exception('Error while reading csv file : {0}'.format(csv_fic,))
-    finally:
-        csv_hand.close()
-
-    return ret
-
-def sqlite_alchemy_feed(csv_fic, db_fic, dat, uq_table):
-    """ This function feeds sqlite database with csv list
-        sqlite_alchemy_feed :: FilePath          ^ -- Csv filepath
-                            -> FilePath          ^ -- SQLite filepath
-                            -> (M a, f b -> M a) ^ -- Tuple composed of datatype and function that produce such data from row
-                            -> [Integer]         ^ -- Uniqueness table
-                            -> IO ()
-    """
-    engine = create_engine('sqlite:///' + db_fic.replace('\\', '\\\\'))
-    Session = sessionmaker(bind=engine)
-    s       = Session()
-    logger.info('Reading configuration files and updating database if needed.')
-    remove_csv_duplicates(csv_fic)
-    with open(csv_fic) as f1:
-        db_count  = s.query(dat[0].nudoss).count()
-        csv_count = get_uq_comb_in_csv(csv_fic, uq_table)
-        logger.debug('Number of queryed feeds           : ' + str(db_count))
-        logger.debug('Number of unique rows in csv file : ' + str(csv_count))
-        if db_count == 0:
-            for row in csv.reader(f1):
-                s.add(dat[1](row))
-        elif csv_count == db_count:
-            logger.info('Nothing to update.')
-        elif csv_count < db_count:
-            logger.info('Csv data is too short. File may not be in sync with db or corrupt.')
-        else:
-            csv_dump = [r for r in csv.reader(f1)]
-            for row in csv_dump[(csv_count - db_count):-1]:
-                try:
-                    s.add(dat[1](row))
-                except:
-                    logger.exception('New data already exist or is corrupt. Update the csv!')
-                    break
-    s.commit()
-
-def dump_sqlite_alchemy_datatype(csv_fic, s, dat):
-    """ This function takes a csv file, a SQLAlchemy session and datatype and dump its column given formating function
-        dump_sqlite_alchemy_datatype :: FilePath
-                                     -> DBSession
-                                     -> M a
-                                     -> IO ()
-    """
-    data = s.query(dat).all()
-
+        log.error('Csv file to be filtered does not exist : %s.', csv_fic)
 
 #==========================================================================
 #0
--- net/shksystem/scripts/rssget.py
+++ net/shksystem/scripts/rssget.py
@@ -24,27 +24,25 @@ import sys
 import csv
 import logging
 import re
-# installed
-import keyring
 import configparser
+# installed
 import feedparser
+import transmissionrpc
 #import requests
-from sqlalchemy                     import create_engine, Column, Integer, String, Boolean, ForeignKey, UniqueConstraint
-from sqlalchemy.ext.declarative     import declarative_base
-from sqlalchemy.orm                 import sessionmaker, relationship
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 # custom
-from net.shksystem.common.error     import FileNotFound
-from net.shksystem.common.utils     import get_current_timestamp, sqlite_alchemy_feed
-from net.shksystem.common.send_mail import SendMail
-from net.shksystem.common.logic     import Switch
+from net.shksystem.common.error import FileNotFound
+from net.shksystem.common.utils import get_current_timestamp, remove_csv_duplicates
 
 #==========================================================================
 # Environment/Static variables
 #==========================================================================
 
-logger    = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 base_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
-Base      = declarative_base()
+Base = declarative_base()
 
 #==========================================================================
 # Classes/Functions
@@ -53,59 +51,61 @@ Base      = declarative_base()
 # -- Models
 # -------------------------------------------------------------------------
 
-class Rule(Base):
-    __tablename__  = 'rules'
-    nudoss         = Column(Integer, primary_key=True)
-    is_active      = Column(Boolean, default=False)
-    name           = Column(String, nullable=False)
-    dest           = Column(String, nullable=False)
-    regex          = Column(String, nullable=False)
-    feed           = Column(Integer, ForeignKey('feeds.nudoss'))
-    dlleds         = relationship('DLLed', backref='dlleds')
-    __table_args__ = (UniqueConstraint('dest', 'regex', 'feed',  name='uq_rule'),)
-
-    def __init__(self, name, dest, regex, feed_id):
-        self.name  = name
-        self.dest  = dest
-        self.regex = regex
-        self.feed  = feed_id
-
 class Feed(Base):
-    __tablename__  = 'feeds'
-    nudoss         = Column(Integer, primary_key=True)
-    feed_url       = Column(String, nullable=True, unique=True)
-    rules          = relationship('Rule', backref='rules')
-    last_checked   = Column(String, nullable=True)
+    __tablename__ = 'feeds'
+    nudoss = Column(Integer, primary_key=True)
+    feed_url = Column(String, nullable=True, unique=True)
+    rules = relationship('Rule', backref='rules')
+    last_checked = Column(String, nullable=True)
 
     def __init__(self, feed_url):
         self.feed_url     = feed_url
         self.last_checked = get_current_timestamp()
 
-class DLLed(Base):
-    __tablename__  = 'dlleds'
-    nudoss         = Column(Integer, primary_key=True)
-    filename       = Column(String, nullable=False)
-    rule           = Column(Integer, ForeignKey('rules.nudoss'))
+class Rule(Base):
+    __tablename__ = 'rules'
+    nudoss = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    regex = Column(String, nullable=False)
+    feed = Column(Integer, ForeignKey('feeds.nudoss'))
+    __table_args__ = (UniqueConstraint('regex', 'feed',  name='uq_rule'),)
 
-    def __init__(self, filename, rule_id):
-        self.filename = filename
-        self.rule     = rule_id
+    def __init__(self, name, regex, feed_id):
+        self.name = name
+        self.regex = regex
+        self.feed = feed_id
+
+class DLLed(Base):
+    __tablename__ = 'dlleds'
+    nudoss = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+
+    def __init__(self, title):
+        self.title = title
 
 # -- PROCESSES
 # -------------------------------------------------------------------------
 def run_feeds():
 
-    conf_dir  = os.path.abspath('../etc/{0}'.format(base_name,))
-    feed_fic  = os.path.join(conf_dir, 'feeds.csv')
-    rule_fic  = os.path.join(conf_dir, 'rules.csv')
-    db_fic    = os.path.join(conf_dir, '{0}.db'.format(base_name,))
-    logger.debug('Configuration directory : ' + conf_dir)
-    logger.debug('Feed list csv           : ' + feed_fic)
-    logger.debug('Rule list csv           : ' + rule_fic)
-    logger.debug('SQLite database path    : ' + db_fic)
+    conf_dir = os.path.abspath('../etc/{0}'.format(base_name,))
+
+    conf_file = os.path.join(conf_dir, '{0}.ini'.format(base_name,))
+    if not os.path.isfile(conf_file):
+        logger.error('Configuration ini %s does not exist.', conf_file)
+        raise FileNotFound
+    config = configparser.ConfigParser()
+    config.read(conf_file)
+
+    db_fic = os.path.join(conf_dir, '{0}.db'.format(base_name,))
     engine = create_engine('sqlite:///' + db_fic.replace('\\', '\\\\'))
 
-    logger.info('#### Processing all feeds. ####')
+    feed_fic = os.path.join(conf_dir, 'feeds.csv')
+    rule_fic = os.path.join(conf_dir, 'rules.csv')
+
+    logger.debug('Conf fic is  : %s', conf_file)
+    logger.debug('DB fic is    : %s', db_fic)
+    logger.debug('Feed fic is  : %s', feed_fic)
+    logger.debug('Rule fic is  : %s', rule_fic)
 
     if not os.path.isfile(db_fic):
         logger.info('Database file does not exist. Creating it.')
@@ -113,73 +113,36 @@ def run_feeds():
             logger.error('Required data files do not exist. Please create them and run again.')
             raise FileNotFound
         Base.metadata.create_all(engine)
-    sqlite_alchemy_feed(feed_fic, db_fic, (Feed, lambda x: Feed(x[0])), [0])
-    sqlite_alchemy_feed(rule_fic, db_fic, (Rule, lambda x: Rule(x[0], x[1], x[2], int(x[3]))), [1, 2, 3])
+        init_session = sessionmaker(bind=engine)
+        session = init_session()
+        remove_csv_duplicates(feed_fic)
+        with open(feed_fic) as feed_hand:
+            for row in csv.reader(feed_hand):
+                session.add(Feed(row[0]))
+        remove_csv_duplicates(rule_fic)
+        with open(rule_fic) as rule_hand:
+            for row in csv.reader(rule_hand):
+                session.add(Rule(row[0], row[1], int(row[2])))
+        session.commit()
     logger.info('Database is ready for business')
 
-    # logger.info('Checking data.')
-    # if not os.path.isfile(db_fic):
-    #     logger.info('Database file does not exist. Creating it.')
-    #     if not (all(map(os.path.isfile, [feed_fic, rule_fic]))):
-    #         logger.error('Required data files do not exist. Please create them and run again.')
-    #         raise FileNotFound
-    #     Base.metadata.create_all(engine)
-    #     logger.info('Creating session to feed database.')
-    #     Session = sessionmaker(bind=engine)
-    #     s       = Session()
-    #     logger.info('Reading configuration files and inserting into database.')
-    #     remove_csv_duplicates(feed_fic)
-    #     with open(feed_fic) as f1:
-    #         for w1 in csv.reader(f1):
-    #             s.add(Feed(w1[0]))
-    #     remove_csv_duplicates(rule_fic)
-    #     with open(rule_fic) as f2:
-    #         for w2 in csv.reader(f2):
-    #             s.add(Rule(w2[0], w2[1], w2[2], int(w2[3])))
-    #     s.commit()
-    # else:
-    #     if not (all(map(os.path.isfile, [feed_fic, rule_fic]))):
-    #         logger.info('Required data files do not exist. Database does not need updates.')
-    #     else:
-    #         Session = sessionmaker(bind=engine)
-    #         s       = Session()
-    #         logger.info('Reading configuration files and updating database if needed.')
-    #         remove_csv_duplicates(feed_fic)
-    #         with open(feed_fic) as f1:
-    #             data      = [i for i in csv.reader(f1)]
-    #             row_count = len(data)
-    #             db_count  = s.query(Feed.nudoss).count()
-    #             logger.debug('1001102 - 2 - Feeds relative informations =')
-    #             logger.debug('Number of rows in csv file : ' + str(row_count))
-    #             logger.debug('Number of queryed feeds    : ' + str(db_count))
-    #             if  row_count == db_count:
-    #                 logger.info('Nothing to update.')
-    #             else:
-    #                 for row in data:
-    #                     if s.query(Feed).filter_by(feed_url=row[0]).first() is None:
-    #                         s.add(Feed(row[0]))
-    #         remove_csv_duplicates(rule_fic)
-    #         with open(rule_fic) as f2:
-    #             data      = [i for i in csv.reader(f2)]
-    #             row_count = len(data)
-    #             db_count  = s.query(Rule.nudoss).count()
-    #             logger.debug('1001102 - 3 - Rules relative informations =')
-    #             logger.debug('Number of rows in csv file : ' + str(row_count))
-    #             logger.debug('Number of queryed feeds    : ' + str(db_count))
-    #             if  row_count == db_count:
-    #                 logger.info('Nothing to update.')
-    #             else:
-    #                 for row in data:
-    #                     if s.query(Rule).filter( (Rule.dest == row[1]) & (Rule.regex == row[2]) & (Rule.feed == int(row[3])) ).first() is None:
-    #                         s.add(Rule(row[0], row[1], row[2], int(row[3])))
-    #         s.commit()
-    # logger.info('Database is ready for business')
-
     logger.info('Running Rules.')
-    Session  = sessionmaker(bind=engine)
-    s        = Session()
-
-    logger.info('#### Done.')
+    init_session = sessionmaker(bind=engine)
+    session = init_session()
+    dlleds = [x.title for x in session.query(DLLed).all()]
+    rc = transmissionrpc.Client(config.get('transmission', 'host'), config.getint('transmission', 'port'))
+    for feed in session.query(Feed).all():
+        logger.info('Running feed : %s', feed.feed_url)
+        rss = feedparser.parse(feed.feed_url)
+        for rule in feed.rules:
+            logger.info('Running rule : %s', rule.name)
+            for entry in rss.entries:
+                title = entry['title']
+                #logger.debug('Filtering on title : %s', title)
+                if re.match(rule.regex, title.lower()) and (title not in dlleds):
+                    session.add(DLLed(title))
+                    rc.add_torrent(entry['torrent_magneturi'])
+                    logger.info('Torrent %s added.', title)
 
 #==========================================================================
 #0
