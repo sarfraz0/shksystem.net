b010f730fac9644774ab823fbd5e8c588734e911
Sarfraz Kapasi <sarfraz@variablentrerprise.com>
2016-04-04 23:03:20 +0200

    0.1.10.3

--- net/shksystem/db/logit.py
+++ net/shksystem/db/logit.py
@@ -6,7 +6,7 @@ __license__ = 'GPL-3'
 
 # standard
 # installed
-from sqlalchemy import create_engine, Table, Column, Integer, Double, String, \
+from sqlalchemy import create_engine, Table, Column, Integer, Float, String, \
                        ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -28,7 +28,7 @@ class Action(Base):
     cid = Column(Integer, primary_key=True)
     timestamp = Column(String, nullable=False)
     name = Column(String, nullable=False, unique=True)
-    value = Column(Double)
+    value = Column(Float)
     owner = Column(String, nullable=False)
     subcategory_cid = Column(Integer, ForeignKey('subcategories.cid'))
 
@@ -62,6 +62,9 @@ class Flogger(Base):
         self.owner = owner
         self.subcategory = subcategory
 
+    def to_action(self, value=None):
+        return Action(self.name, self.owner, self.subcategory, value)
+
     def to_dict(self):
         ret = {}
         ret['name'] = self.name
@@ -79,6 +82,18 @@ class SubCategory(Base):
     floggers = relationship('Flogger', backref='subcategory')
     category_cid = Column(Integer, ForeignKey('categories.cid'))
 
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+
+    def to_dict(self):
+        ret = {}
+        ret['name'] = self.name
+        ret['actions'] = [x.name for x in self.actions]
+        ret['floggers'] = [x.name for x in self.floggers]
+        ret['category'] = self.category.name
+        return ret
+
 
 class Category(Base):
     __tablename__ = 'categories'
@@ -87,5 +102,53 @@ class Category(Base):
     name = Column(String, nullable=False, unique=True)
     subcategories = relationship('SubCategory', backref='category')
 
+    def __init__(self, name):
+        self.name = name
+
+    def to_dict(self):
+        ret = {}
+        ret['name'] = self.name
+        ret['subcategories'] = [x.name for x in self.subcategories]
+        return ret
+
+
+def init_db(easy_connect):
+    """
+        init_db :: String -> IO ()
+        ==========================
+        Initialize the database schema
+    """
+    engine = create_engine(easy_connect)
+    Base.metadata.create_all(engine)
+
+def populate_db(easy_connect):
+    """
+        populate_db :: String -> IO ()
+        ==============================
+        Initialize the database schema and adds the standard columns
+    """
+    engine = create_engine(easy_connect)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # categories
+    alimentation = Category('ALIMENTATION')
+    sante = Category('SANTE')
+    divertissement = Category('DIVERTISSEMENT')
+    finance = Category('FINANCE')
+    work = Category('WORK')
+    session.add_all([alimentation, divertissement, finance, work])
+    session.commit()
+
+    # subcategories
+    asthme = SubCategory('ASTHME', sante)
+    douleurs = SubCategory('DOULEURS', sante)
+    debit = SubCategory('DEBIT', finance)
+    credit = SubCategory('CREDIT', finance)
+    pomo = SubCategory('POMO', work)
+    timimg = SubCategory('TIME', work)
+    session.add_all([asthme, douleurs, debit, credit, pomo, timimg])
+    session.commit()
 
 #
